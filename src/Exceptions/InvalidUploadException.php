105261ae8693c58cfccbb2f50775e38f692802de
Navneet Rai <navneetrai@gmail.com>
2016-09-06 00:51:02 +0530

    Updated Exception Message

--- src/Exceptions/InvalidUploadException.php
+++ src/Exceptions/InvalidUploadException.php
@@ -6,26 +6,4 @@ use Exception;
  * Generic library-level exception.
  */
 class InvalidUploadException extends Exception{
-	public $message;
-
-	/**
-     * Create a new exception instance.
-     *
-     * @param  string  $data
-     * @param  array  $data
-     * @return void
-     */
-	public function __construct(string $message){
-		$this->message = $message;
-		parent::__construct($message);
-	}
- 	
- 	/**
-     * Get the underlying response instance.
-     *
-     * @return string
-     */
-	public function getMessage(){
-		return $this->message;
-	}
 }
